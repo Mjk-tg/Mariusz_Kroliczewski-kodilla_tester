a7da7dbe52b4e83cae45699aca24721fe0c52d1d
Mariusz Kroliczewski <mjk.tgbox@gmail.com>
2019-12-03 15:06:06 +0100

    Exercise 3.7 rev0

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,26 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        int substractResult = calculator.subtract(a, b);
+        boolean sumCorrect = ResultChecker.assertEquals(13, sumResult);
+        if (sumCorrect) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        boolean substractCorrect = ResultChecker.assertEquals(-3, substractResult);
+        if (substractCorrect) {
+            System.out.println("Metoda substract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda substract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+    }
+}
