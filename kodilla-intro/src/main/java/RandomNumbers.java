cf1fa49ead4e176e10a181edc360e9b30f9f2403
Mariusz Kroliczewski <mjk.tgbox@gmail.com>
2019-12-06 13:14:55 +0100

    Exercises_Mod_3.3-2_rev1

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -2,26 +2,21 @@ import java.util.Random;
 
 public class RandomNumbers {
     public static void main(String[] args) {
+
+        int smallestValue = getSmallestValue (5000, 30);
+        System.out.println("wartość najmniejsza = " + smallestValue);
+
+        int largestValue = getLargestValue(5000,30);
+        System.out.println( "wartość największa = " + largestValue);
+
     }
 
-    public int getCountOfRandomNumber(int max, int range ) {
-        Random random = new Random();
-        int result = 0;
-        int sum = 0;
-        while (sum < max) {
-            int temp = random.nextInt(range);
-            sum = sum + temp;
-            result++;
-        }
-        return result;
-    }
-
-    public int getSmallestValue(int max, int range) {
+    public static int getSmallestValue(int max, int range) {
         Random random = new Random();
         int sum = 0;
-        int smallestValue = range-1;
+        int smallestValue = range;
         while (sum < max) {
-            int temp = random.nextInt(range);
+            int temp = random.nextInt(range+1);
             sum = sum + temp;
             if (temp < smallestValue) {
                 smallestValue = temp;
@@ -29,31 +24,18 @@ public class RandomNumbers {
         }
         return smallestValue;
     }
-    public int getLargestValue(int max, int range) {
+    public static int getLargestValue(int max, int range) {
         Random random = new Random();
         int sum = 0;
         int largestValue = 0;
         while (sum < max) {
-            int temp = random.nextInt(range);
+            int temp = random.nextInt(range+1);
             sum = sum + temp;
             if (temp > largestValue) {
                 largestValue = temp;
             }
-            System.out.println( "wartość największa = " + largestValue );
         }
         return largestValue;
-    }
-    public void getResults() {
-
-
-        int result = getCountOfRandomNumber(5000,31);
-
-        int smallestValue = getSmallestValue (5000, 31);
-
-        int largestValue = getLargestValue(5000,31);
-
-        System.out.println("liczba losowań = " + result + "wartość najmniejsza = " + smallestValue + "wartość największa = " );
-
     }
 
 }
