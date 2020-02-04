b955e32e0e14cf29aa5f782e198f34280b4f352a
Mariusz Kroliczewski <mjk.tgbox@gmail.com>
2020-02-04 16:38:41 +0100

    Exercise_Mod_5.3_rev.0

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,12 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public final class Task {
+    String title;
+    int duration;
+
+    public Task(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+}
