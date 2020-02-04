25c33970df7c6a986408f2a55a3726ccb4a48d32
Mariusz Kroliczewski <mjk.tgbox@gmail.com>
2020-02-04 13:57:51 +0100

    Exercises_Mod_4.2_rev1

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/arrays/homework/CarAplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/arrays/homework/CarAplication.java
@@ -12,7 +12,8 @@ public class CarAplication {
 
 
     public static void main(String[] args) {
-        Car[] cars = new Car[15];
+        Random random = new Random();
+        Car[] cars = new Car[random.nextInt(15)+1];
         for (int i = 0; i < cars.length; i++)
             cars[i] = drawCar();
 
