6e558932006ec85ff39494816552e6fda8b8937f
Mariusz Kroliczewski <mjk.tgbox@gmail.com>
2020-02-11 10:36:45 +0100

    Exercise_Mod_4.3_rev1

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -1,7 +1,10 @@
 package com.kodilla.collections.lists.homework;
 
 import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
 import com.kodilla.collections.interfaces.homework.Fiat;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Opel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,21 +12,27 @@ import java.util.List;
 
 public class CarsListApplication {
     public static void main(String[] args) {
-        List<Fiat> car = new ArrayList<>();
-        Fiat fiat125 = new Fiat(165,13,9);
-        Fiat fiat126 = new Fiat(135,15,10);
-        Fiat fiat127 = new Fiat(175,11,8);
-        Fiat fiat131 = new Fiat(190,9,8);
-        Fiat fiat132 = new Fiat(185,10,9);
+        List<Car> car = new ArrayList<>();
+        Car fiat125 = new Fiat(165,13,9);
+        Car fiat126 = new Fiat(135,15,10);
+        Car fiat127 = new Fiat(175,11,8);
+        Car fiat131 = new Fiat(190,9,8);
+        Car fiat132 = new Fiat(185,10,9);
+        Car fordGranada = new Ford(157,12,7);
+        Car opelAscona = new Opel(155,13,8);
+
+
         car.add(fiat125);
         car.add(fiat126);
         car.add(fiat127);
         car.add(fiat131);
         car.add(fiat132);
+        car.add(fordGranada);
+        car.add(opelAscona);
 
-        for (Fiat fiat: car
+        for (Car auto: car
              ) {
-            CarUtils.describeCar(fiat);
+            CarUtils.describeCar(auto);
         }
         System.out.println(" Elementów w kolekcji " + car.size());
 
@@ -31,9 +40,9 @@ public class CarsListApplication {
         car.remove(fiat131);
 
 
-        for (Fiat fiat: car
+        for (Car auto: car
         ) {
-            CarUtils.describeCar(fiat);
+            CarUtils.describeCar(auto);
         }
         System.out.println(" Elementów w kolekcji " + car.size());
 
