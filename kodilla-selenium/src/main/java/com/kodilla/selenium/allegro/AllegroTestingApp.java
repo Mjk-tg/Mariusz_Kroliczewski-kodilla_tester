e1fe8c225166a00de31d234a38bf9f0494f6eb8f
Mariusz Kroliczewski <mjk.tgbox@gmail.com>
2020-03-04 12:06:36 +0100

    Exercise_Mod_13.4_rev0

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,6 +1,5 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,20 +11,20 @@ public class AllegroTestingApp {
         System.setProperty("webdriver.chrome.driver", "e:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
-        driver.g
 
 //        Alert alert = driver.switchTo().alert();
 //        alert.accept();
 
-        WebElement allegroInfo = driver.findElement(By.xpath("//html/body/div[2]/div[6]/div/div[2]/div/div[2]/button[2]"));
-        allegroInfo.submit();
+        WebElement allegroInfo = driver.findElement(By.xpath("//*[@class=\"_13q9y _8hkto _11eg6 _7qjq4 _ey68j\"]"));
+        allegroInfo.click();
+
 
         WebElement allegroProductSelection = driver.findElement(By.xpath("//*[@data-prototype-id=\"allegro.metrumHeader.search\"]/div/form/input"));
         allegroProductSelection.sendKeys(("Mavic mini"));
-        allegroProductSelection = driver.findElement(By.xpath("//*[@class=class=\"_r65gb _1fwkl\"]/select[@class=\"_d25db_2P-5I _1h7wt _k70df _7qjq4\"]"));
+        allegroProductSelection = driver.findElement(By.xpath("//*[@class=\"_d25db_2P-5I _1h7wt _k70df _7qjq4\"]"));
         Select categorySelect = new Select(allegroProductSelection);
-        categorySelect.selectByValue("/kategoria/elektronika");
-        allegroProductSelection = driver.findElement(By.xpath("//button[@class=\"_d25db_1t06j _13q9y _8tsq7 _1q2ua\"]/"));
+        categorySelect.selectByVisibleText("Elektronika");
+        allegroProductSelection = driver.findElement(By.xpath("//*[@class=\"_d25db_1t06j _13q9y _8tsq7 _1q2ua\"]"));
         allegroProductSelection.submit();
     }
 }
